7351a9a385860baf87f4680f6a3195a2d6f977b1
Alex761944 <christen.alexander@web.de>
2026-01-05 15:59:34 +0100

    Work in progress

--- PROJECT/travelRecommendation/travel_recommendation.js
+++ PROJECT/travelRecommendation/travel_recommendation.js
@@ -1,13 +1,15 @@
 const searchButtonElement = document.getElementById("search-button");
 const searchInputElement = document.getElementById("search-input");
 
+let travelData = [];
+
 searchButtonElement.addEventListener("click", () => {
   const searchInputValue = searchInputElement.value;
-  console.log(searchInputValue);
 });
 
 async function loadData() {
   const response = await fetch("travel_recommendation_api.json");
-  const data = await response.json();
+  travelData = await response.json();
 }
+
 loadData();
